4d4ef5d7351ca4496a4c3f6ec645d83ebf8fc7d5
Dan Currotto <dcurrotto@bstglobal.com>
2018-12-02 15:54:54 -0500

    Updating CONFIG_VERSION.

---
+++ src/dotnet-core-simple-web-ui/UpdateConfigMapAndSecret.ps1
@@ -0,0 +1,17 @@
+﻿
+
+$configMapPath = "./Configuration/ConfigMaps/appsettings.configmaps.json"
+$configMapJson = Get-Content $configMapPath | ConvertFrom-Json
+$now = Get-Date
+$configMapJson.ConfigMaps.IdentityProviderClientId = -join("configMap", " ", $now.ToUniversalTime().ToString('HH:mm:ss'))
+$configMapJson | ConvertTo-Json | set-content $configMapPath
+
+
+$secretPath = "./Configuration/Secrets/appsettings.secrets.json"
+$secretJson = Get-Content $secretPath | ConvertFrom-Json
+$now = Get-Date
+$secretJson.Secrets.IdentityProviderSecret = -join("secret", " ", $now.ToUniversalTime().ToString('HH:mm:ss'))
+$secretJson | ConvertTo-Json | set-content $secretPath
+
+
+
