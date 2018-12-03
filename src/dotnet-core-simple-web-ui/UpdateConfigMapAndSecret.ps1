738a596ce08f01eed60b6bd28ef5ed0e4d1130d1
Dan Currotto <dcurrotto@bstglobal.com>
2018-12-03 18:26:02 -0500

    Updating CONFIG_VERSION...

--- src/dotnet-core-simple-web-ui/UpdateConfigMapAndSecret.ps1
+++ src/dotnet-core-simple-web-ui/UpdateConfigMapAndSecret.ps1
@@ -1,15 +1,15 @@
 ﻿Param
 (
-    [Parameter(Mandatory=$true)] [string]$IdentityProviderSecret, [Parameter(Mandatory=$true)] [string]$DropFolder
+    [Parameter(Mandatory=$true)] [string]$IdentityProviderClientId, [Parameter(Mandatory=$true)] [string]$IdentityProviderSecret, [Parameter(Mandatory=$true)] [string]$DropFolder
 )
 write-host "IdentityProviderSecret: $IdentityProviderSecret"
-
+write-host "IdentityProviderClientId: $IdentityProviderClientId"
 write-host "DropFolder: $DropFolder"
 
 $configMapPath = "$DropFolder/Configuration/ConfigMaps/appsettings.configmaps.json"
 $configMapJson = Get-Content $configMapPath | ConvertFrom-Json
 $now = Get-Date
-$configMapJson.ConfigMaps.IdentityProviderClientId = -join("configMap", " ", $now.ToUniversalTime().ToString('HH:mm:ss'))
+$configMapJson.ConfigMaps.IdentityProviderClientId = -join("$IdentityProviderClientId", " ", $now.ToUniversalTime().ToString('HH:mm:ss'))
 $configMapJson | ConvertTo-Json | set-content $configMapPath
 
 
